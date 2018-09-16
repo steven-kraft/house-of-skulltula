67055aa93999d34b13fb45a8893bd8b8b891a5d8
Steven Kraft <steven.kraftjp@gmail.com>
2018-09-15 21:44:05 -0400

    Setup React

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <button className="test">
+        {/* TODO */}
+      </button>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
